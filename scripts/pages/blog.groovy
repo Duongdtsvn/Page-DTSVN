127961ece7fd848c03d193263533b890756787ac
admin admin <evaladmin@example.com>
2025-07-02 08:13:26 +0000

    "User admin wrote content /scripts/pages/blog.groovy"

--- scripts/pages/blog.groovy
+++ scripts/pages/blog.groovy
@@ -1,14 +1,1 @@
-import org.craftercms.sites.editorial.BlogSearchHelper  // ✅ đúng tên
 
-// Lấy tham số từ contentModel
-def categoryKey = contentModel.category_s
-def maxItems = contentModel.max_item_i ?: 10
-
-// Tạo instance của helper
-def blogHelper = new BlogSearchHelper(searchClient, urlTransformationService)
-
-// Gọi đúng tên hàm từ class
-def blogs = blogHelper.getAllBlogs(categoryKey, 0, maxItems)
-
-// Đưa vào templateModel để dùng ở FTL
-templateModel.blogs = blogs
