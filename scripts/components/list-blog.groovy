911829b0d3adc7c95cdcdec09ab6330a89ed6dcd
admin admin <evaladmin@example.com>
2025-07-02 09:28:14 +0000

    "User admin wrote content /scripts/components/list-blog.groovy"

--- scripts/components/list-blog.groovy
+++ scripts/components/list-blog.groovy
@@ -2,7 +2,7 @@
  import org.craftercms.sites.editorial.ProfileUtils
 
 def segment = ProfileUtils.getSegment(profile, siteItemService)
-def category = contentModel.category_s 
+def categories = contentModel.categories_s 
 def maxArticles = contentModel.max_articles_i
 def searchHelper = new SearchHelper(searchClient, urlTransformationService)
 def articles = searchHelper.searchArticles(false, category, segment, 0, maxArticles)
