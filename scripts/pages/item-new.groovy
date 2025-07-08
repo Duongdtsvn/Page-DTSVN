e809ef3113e501d93078ef1c5c8b125541532431
admin admin <evaladmin@example.com>
2025-07-08 09:00:29 +0000

    "User admin wrote content /scripts/pages/item-new.groovy"

--- scripts/pages/item-new.groovy
+++ scripts/pages/item-new.groovy
@@ -1,19 +1,9 @@
-import scripts.libs.CommonLifecycleApi
-
 // Nếu path kết thúc bằng .en thì bỏ .en để lấy file gốc
 def realPath = path
 if (realPath.endsWith('.en')) {
     realPath = realPath.substring(0, realPath.length() - 3)
 }
 
-def contentLifecycleParams = [:]
-contentLifecycleParams.site = site
-contentLifecycleParams.path = realPath
-contentLifecycleParams.user = user
-contentLifecycleParams.contentType = contentType
-contentLifecycleParams.contentLifecycleOperation = contentLifecycleOperation
-contentLifecycleParams.contentLoader = contentLoader
-contentLifecycleParams.applicationContext = applicationContext
-
-def controller = new CommonLifecycleApi(contentLifecycleParams)
-controller.execute()
+// Lấy content object từ contentModelService (API mặc định của CrafterCMS)
+def content = contentModelService.getContentObject(site, realPath)
+return [contentModel: content]
