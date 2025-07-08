dd62b0e62b771aabea417d4faa60a8f8492e9182
admin admin <evaladmin@example.com>
2025-07-08 09:07:16 +0000

    "Delete file /scripts/pages/item-new.groovy"

--- scripts/pages/item-new.groovy
+++
@@ -1,9 +0,0 @@
-// Nếu path kết thúc bằng .en thì bỏ .en để lấy file gốc
-def realPath = path
-if (realPath.endsWith('.en')) {
-    realPath = realPath.substring(0, realPath.length() - 3)
-}
-
-// Lấy content object từ contentModelService (API mặc định của CrafterCMS)
-def content = contentModelService.getContentObject(site, realPath)
-return [contentModel: content]
